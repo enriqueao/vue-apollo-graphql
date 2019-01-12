db9c8f52c012e412b53a3865386d5a25dcab46bc
Enrique Aguilar <enriqueao96@gmail.com>
2019-01-12 11:36:29 -0600

    fix styles for search bar

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="container">
     <div class="input-group mb-3 col-sm">
-        <input v-model="description" placeholder="Busca un Producto" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+        <input @input="search" v-model="description" placeholder="Busca un Producto" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
     </div>
     <!-- <p>
       {{product}}
     </p> -->
-    <div class="row">
-        <div v-for="item in product" v-bind:key="item._id" class="col-sm">
+    <div v-if="loading" class="row justify-content-center">
+        <img src="../assets/loading.svg" class="col-6">
+    </div>
+    <div v-if="!loading" class="row">
+        <div v-for="item in product" v-bind:key="item._id" class="col-md-3">
             <div class="card">
-                <img :src="item.pic" class="card-img-top" alt="...">
+                <img :src="item.pic" class="card-img-top" :alt="item.upc">
                 <div class="card-body">
-                    <h5 class="card-title">{{ item.prices[0].market.marketName }}</h5>
+                    <h6 class="card-title">{{ item.prices[0].market.marketName }}</h6>
                     <h5 class="card-title">${{ item.prices[0].price }}</h5>
                     <p class="card-text">{{ item.description }}</p>
                 </div>
@@ -57,11 +60,17 @@ export default {
         return {
             // Initialize your apollo data
             description: '',
+            loading: false
         }
     },
     methods: {
         search: function () {
-            this.msg = this.name
+            setTimeout(()=>{
+                this.loading = true
+            },800)
+            setTimeout(()=>{
+                this.loading = false
+            },2400)
         }
     }
 }
