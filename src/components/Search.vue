35e24429c1e7c81f9a2c7a21ed0eff357043a993
enriqueao <enriqueao96@gmail.com>
2019-01-13 21:19:14 -0600

    some bugs fixed

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,28 +1,66 @@
 <template>
-  <div class="container">
-    <div class="input-group mb-3 col-sm">
-        <input @input="search" v-model="description" placeholder="Busca un Producto" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-    </div>
-    <!-- <p>
-      {{product}}
-    </p> -->
-    <div v-if="loading" class="row justify-content-center">
-        <img src="../assets/loading.svg" class="col-6">
-    </div>
-    <div v-if="!loading" class="row">
-        <div v-for="item in product" v-bind:key="item._id" class="col-md-3">
-            <div class="card">
-                <img :src="item.pic" class="card-img-top" :alt="item.upc">
-                <div class="card-body">
-                    <h6 class="card-title">{{ item.prices[0].market.marketName }}</h6>
-                    <h5 class="card-title">${{ item.prices[0].price }}</h5>
-                    <p class="card-text">{{ item.description }}</p>
+  <div style="heigth: 100%">
+    <nav class="navbar navbar-light bg-light">
+        <div class="navbar-brand">
+            <img src="../assets/laptop.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+            Buscador de Productos
+        </div>
+    </nav>
+    <div class="container-fluid" style="heigth: 70%">
+        <div class="row justify-content-md-center">
+            <div class="input-group mb-3 col-6 align-items-center" style="margin-top: 40px">
+                <input @input="search" v-model="description" placeholder="Busca un Producto" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+            </div>
+        </div>
+        <div class="row justify-content-md-center">
+            <div v-if="loading" class="d-flex align-items-center">
+                <strong>Buscando... </strong>
+                <div class="spinner-border" role="status" aria-hidden="true"></div>
+            </div>
+            <div v-if="product.length == 0 && !loading" class="d-flex align-items-center">
+                <strong>Sin Resultados ☹️</strong>
+            </div>
+        </div>
+        <div v-if="!loading" class="row">
+            <div v-for="item in product" v-bind:key="item._id" class="col-md-2 col-sm-3 col-6 col-lg-2" style="margin-top: 20px">
+                <div class="card">
+                    <img :src="item.pic" class="card-img-top" :alt="item.upc">
+                    <div class="card-body">
+                        <p class="card-text">{{ item.description }}</p>
+                        <div class="card">
+                            <ul v-for="price in item.prices" v-bind:key="price._id" class="list-group list-group-flush">
+                                <li class="list-group-item"><b>{{ price.market.marketName }}: </b>${{ price.price }}</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <footer class="page-footer font-small bg-dark">
+        <div class="footer-copyright text-center py-3 text-white">© 2018 Copyright:
+            <a href="https://github.com/enriqueao">Enrique Aguilar</a>
+        </div>
+    </footer>
   </div>
 </template>
+
+
+<style>
+    footer {
+        top: 170vh;
+        width: 100%;
+        height: 4rem;
+        bottom: 0;
+        position: absolute;
+        margin-top: 20px;
+    }
+    .container-fluid{
+        height: 100%;
+    }
+</style>
+
+
 
 <script>
 import gql from 'graphql-tag'
@@ -67,10 +105,10 @@ export default {
         search: function () {
             setTimeout(()=>{
                 this.loading = true
-            },800)
+            }, 800)
             setTimeout(()=>{
                 this.loading = false
-            },2400)
+            }, 2400)
         }
     }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Search from '@/components/Search'
 
 Vue.use(Router)
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'Search',
       component: Search
-    },
-    {
-      path: '/hello',
-      name: 'HelloWorld',
-      component: HelloWorld
     }
   ]
 })
